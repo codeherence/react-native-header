3b1aa742dce759485eb702da9fd230fbc6798305
e-younan <me@eyounan.com>
2023-03-23 17:34:16 -0400

    feat: added section list support

--- src/components/containers/index.ts
+++ src/components/containers/index.ts
@@ -2,6 +2,7 @@ export { default as ScalingView } from './ScalingView';
 export { default as FadingView } from './FadingView';
 export { default as ScrollViewWithHeaders } from './ScrollView';
 export { default as FlatListWithHeaders } from './FlatList';
+export { default as SectionListWithHeaders } from './SectionList';
 export type {
   ScrollHeaderProps,
   ScrollLargeHeaderProps,
