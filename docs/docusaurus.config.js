693d7f7f362644d7015475c144bba7258e677d71
e-younan <me@eyounan.com>
2023-03-26 17:46:40 -0400

    docs: update head metadata for website

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'React Native Header',
-  tagline: 'A better header',
+  tagline: 'A simple, yet powerful, React Native Header component library.',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
