408782f0f2af1b3f42e9e9f8efc8179ea4b5970f
e-younan <me@eyounan.com>
2023-03-26 23:46:26 -0400

    chore: add explicit exports instead of wildcard

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-export * from './components';
+export {
+  Header,
+  LargeHeader,
+  FadingView,
+  ScalingView,
+  ScrollViewWithHeaders,
+  FlatListWithHeaders,
+  SectionListWithHeaders,
+} from './components';
+
+export type {
+  HeaderProps,
+  LargeHeaderProps,
+  ScrollHeaderProps,
+  ScrollLargeHeaderProps,
+  SharedScrollContainerProps,
+} from './components';
